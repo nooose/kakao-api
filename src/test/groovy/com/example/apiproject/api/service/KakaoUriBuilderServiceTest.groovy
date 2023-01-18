d506372c133791a45b8f30ee66324a7f17547608
nooose <hd15807@gmail.com>
2023-01-18 23:32:31 +0900

    Spock 테스트 케이스 작성

---
+++ src/test/groovy/com/example/apiproject/api/service/KakaoUriBuilderServiceTest.groovy
@@ -0,0 +1,26 @@
+package com.example.apiproject.api.service
+
+import spock.lang.Specification
+
+import java.nio.charset.StandardCharsets
+
+class KakaoUriBuilderServiceTest extends Specification {
+
+    private KakaoUriBuilderService kakaoUriBuilderService
+
+    def setup() {
+        kakaoUriBuilderService = new KakaoUriBuilderService()
+    }
+
+    def "buildUriByAddressSearch - 한글 파라미터의 경우 정상적으로 인코딩"() {
+        given:
+        String address = "서울 송파구"
+
+        when:
+        def uri = kakaoUriBuilderService.buildUriByAddressSearch(address)
+        def decodedResult = URLDecoder.decode(uri.toString(), StandardCharsets.UTF_8)
+
+        then:
+        decodedResult == "https://dapi.kakao.com/v2/local/search/address.json?query=서울 송파구"
+    }
+}
